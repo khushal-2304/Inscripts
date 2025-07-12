de59704ccdc348a6d6942d2caf170d0289c4877c
khushal-2304 <sharmakhushal2001@gmail.com>
2025-07-12 22:31:33 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { SpreadsheetView } from "../components/spreadsheet-view"
+
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SpreadsheetView />
+    </div>
+  )
+}
