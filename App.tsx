b428d6dc939ca2e56c32f476344e0aae496abe98
khushal-2304 <sharmakhushal2001@gmail.com>
2025-07-12 14:23:19 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SpreadsheetView } from './components/spreadsheet-view';
+
+
+function App() {
+  return (
+    <div className="p-1">
+     
+      <SpreadsheetView></SpreadsheetView>
+    </div>
+  );
+}
+
+export default App;
